97a7895d4cbafab092563834b987a6df5b3d6d4b
Christina Takara <christina.takara@gmail.com>
2025-02-13 18:24:29 -0600

    added contact form functionality

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # portfolio
+## Introduction
+The overall site is created with HTML, CSS, and minimal JavaScript.  Examples of more advanced programming skills and technologies can be found in the sample projects.
+
+## Technologies
+
+## Resources
+[EmailJS](https://www.emailjs.com/docs/tutorial/overview/)
--- scripts/script.js
+++ scripts/script.js
@@ -6,18 +6,54 @@ toggleMode.addEventListener('click', (evt) => {
     evt.preventDefault();
     body.classList.toggle('dark-mode');
     let childNodes = body.children;
-    console.log(childNodes);
+    // console.log(childNodes);
     for (let child of childNodes ) {
-        console.log(child.tagName);
+        // console.log(child.tagName);
         if (child.tagName !== 'SCRIPT') child.classList.toggle('dark-mode');
     }
 
     // toggleMode.classList.toggle('dark-mode');
-    console.log(body.classList)
+    // console.log(body.classList)
     toggleMode.innerHTML = body.classList.contains('dark-mode') ? '<i class="fa-solid fa-circle-half-stroke"></i> Light Mode ' : '<i class="fa-solid fa-circle-half-stroke"></i> Dark Mode ';
 })
 
 function toggleMenu () {
     let navEl = document.getElementById("nav");
     navEl.classList.toggle('responsive');
+}
+
+function cancelEmail() {
+    let formData = document.getElementById('contact-form');
+    formData.reset();
+    let contactEl = document.getElementById('contact');
+    contactEl.classList.add('hide');
+}
+
+function showContact() {
+    let contactEl = document.getElementById('contact');
+    contactEl.classList.remove('hide');
+}
+
+(function () {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init({
+        publicKey: "KZf90ecqBPS-S28XJ",
+    });
+})();
+
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('profile_service', 'template_vggc9ya', this)
+            .then(() => {
+                // console.log('SUCCESS!');
+                alert('email sent successfully');
+                cancelEmail();
+            }, (error) => {
+                // console.log('FAILED...', error);
+                alert('Sorry, the message failed to send.')
+                cancelEmail();
+            });
+    });
 }
